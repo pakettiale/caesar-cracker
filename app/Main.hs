95cde8807383b684b858658de47d516fd679a7ad
Aleksanteri Paakkinen <aleksanteri.paakkinen@gmail.com>
2017-08-14 21:51:54 +0300

    Initial version of the cipher.

--- app/Main.hs
+++ app/Main.hs
@@ -1,6 +1,42 @@
 module Main where
 
-import Lib
+import System.IO
+import Data.Ord
+import Data.List
+import Data.Tuple
+import Data.Maybe
+import Control.Exception.Base
+
+letters :: [Char]
+letters = "abcdefghijklmnopqrstuvwxyz"
+
+countAppearance :: [Char] -> Char -> (Int, Char)
+countAppearance t s = (length $ elemIndices s t, s)
+
+findE :: String -> Char
+findE t =  snd . maximum $ fmap (countAppearance t) letters
+
+transChar :: Int -> Char -> Char
+transChar d c
+  | elem c letters = letters !! (((+) (0-d) $ fromJust $ elemIndex c letters) `mod` 26)
+  | otherwise      = id c
+
+decryptWithGuess :: String -> String
+decryptWithGuess t =  fmap (transChar $ -4 +( fromJust (elemIndex (findE t) letters))) t
+
+encryptWith ::Int -> String -> String
+encryptWith d text =  fmap (transChar (-d)) text
+
+decryptWith ::Int -> String -> String
+decryptWith d text =  fmap (transChar d) text
 
 main :: IO ()
-main = someFunc
+main = do
+  putStrLn "File?"
+  filename <- getLine
+  file <- (readFile filename) `catch` (\e -> do let err = show (e :: IOException)
+                                                hPutStr stderr ("Warning: " ++ filename ++ ": " ++ err)
+                                                return "")
+  let t = decryptWithGuess file
+  putStrLn t
+  putStrLn . encryptWith 5 $ t
